525df1b9083be0529b732ebf55a21c0dbc143e04
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-03 11:22:17 +0100

    Added my fetched products to its component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { CardMedia, Card, CardActions,CardContent, Typography, IconButton } from '@mui/material'
-import { AddShoppingCart } from '@mui/icons-material'
-import Logo from "../../assets/mobile.jpg"
+import React from 'react';
+import { CardMedia, Card, CardActions, CardContent, Typography, IconButton } from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+import Logo from '../../assets/mobile.jpg';
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
+	console.log(product);
+	const { price: { formatted, formatted_with_symbol }, name, description } = product;
+	return (
+		<Card className="max-w-full">
+			<CardMedia image={Logo} title={product.name} style={{ height: 0, paddingTop: '56.25%' }} />
 
-  return (
-    <Card className = "max-w-full">
-        <CardMedia image = {Logo} title = {product.name} style = {{height : 0, paddingTop: '56.25%'}}/>
+			<CardContent>
+				<div className="flex justify-between">
+					<Typography variant={'h6'} gutterBottom>
+						{name}
+					</Typography>
 
-        <CardContent>
-         <div className= 'flex justify-between'>
-            <Typography variant = {"5"} gutterBottom>
-                {product.name}
-            </Typography>
+					<Typography variant={'h6'}>{formatted_with_symbol}</Typography>
+				</div>
 
-            <Typography variant = {"h5"}>
-            {product.price}
-            </Typography>
-         </div>
+				<Typography
+					variant={'body2'}
+					color={'textSecondary'}
+					dangerouslySetInnerHTML={{ __html: description }}
+				/>
+			</CardContent>
 
-         <Typography variant = {"body2"} color = {"textSecondary"}>
-           {product.desc}
-         </Typography>
-        </CardContent>
+			<CardActions disableSpacing className="flex justify-end">
+				<IconButton aria-label="add to cart">
+					<AddShoppingCart />
+				</IconButton>
+			</CardActions>
+		</Card>
+	);
+};
 
-        <CardActions disableSpacing className = 'flex justify-end'>
-          <IconButton aria-label='add to cart'>
-            <AddShoppingCart />
-          </IconButton>
-        </CardActions>
-
-    </Card>
-  )
-}
-
-export default Product
+export default Product;
