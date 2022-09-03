3a72ab181da83049f8716eb18d52b8ae3b5c5324
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-09-03 10:46:26 +0100

    Test running my commerce Keys and api fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Products, NavBar } from './components'
-import {commerce} from "./lib/commerce"
+import commerce from "./lib/commerce"
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -8,8 +8,14 @@ const App = () => {
   const fetchProducts = async () => {
       // destructured my data from my await result
     const {data} = await commerce.products.list();
+    setProducts(data);
   }
-  
+
+  useEffect (() =>{
+    fetchProducts();
+  },[])
+
+  console.log(products)
 
   return (
     <div className=''>
